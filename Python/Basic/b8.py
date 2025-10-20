b4a30ebd6ce8483fd21924f8ee37efb6f4de7327
haachama05 <fbkhanhfb@gmail.com>
2025-10-20 16:21:08 +0700

    b8

---
+++ Python/Basic/b8.py
@@ -0,0 +1,5 @@
+# Nhập vào số thực a, kiểm tra xem a có phải là số nguyên hay không, nếu có thì in ra True, ngược lại in ra False
+a = float(input("Nhap a: "))
+b = round(a)  # lam tron
+print(b)
+print(a == b)
