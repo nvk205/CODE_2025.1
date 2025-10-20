f52673b05207abe8701f993a71c90321095b2265
haachama05 <fbkhanhfb@gmail.com>
2025-10-20 10:09:17 +0700

    b5

---
+++ Python/Basic/b5.py
@@ -0,0 +1,8 @@
+# Nhập vào số nguyên a, nếu a là số chia hết cho 3 và nằm trong khoảng từ 50 - 100 thì in ra True, ngược lại in ra False
+a = int(input("Nhap so nguyen a: "))
+if 50 <= a and a <= 100 and a % 3 == 0:
+    print("True")
+else:
+    print("False")
+
+print(50 <= a <= 100 and a % 3 == 0)  # clean
