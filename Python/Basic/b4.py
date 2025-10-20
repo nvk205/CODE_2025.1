18c75e3b744133e9eb4f65902000478da7c6512c
haachama05 <fbkhanhfb@gmail.com>
2025-10-20 09:28:51 +0700

    b4

---
+++ Python/Basic/b4.py
@@ -0,0 +1,7 @@
+# Nhập vào một số nguyên a, nếu a chia hết cho 2 thì in ra True, ngược lại in ra False
+a = int(input("Nhap so nguyen a: "))
+if(a%2==0):
+    print("True")
+
+else:
+    print("False")
