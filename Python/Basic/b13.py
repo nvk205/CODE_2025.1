b5def561660a8f4a3f15d35b74f04777f1882d4a
haachama05 <fbkhanhfb@gmail.com>
2025-10-21 19:31:32 +0700

    b13

---
+++ Python/Basic/b13.py
@@ -0,0 +1,6 @@
+# Nhập vào số nguyên dương a, nếu a lớn hơn 10 thì ta in ra đây là số lớn hơn 10
+a = int(input("Nhap so nguyen a: "))
+if a > 10:
+    print("So nguyen lon hon 10")
+else:
+    print("So nguyen khong lon hon 10")
