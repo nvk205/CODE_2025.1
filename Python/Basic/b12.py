3ac3c8524a8a1d0d836b41ba9878d6c8cfa4dc4a
nvk205 <nvk.khanh205@gmail.com>
2025-10-21 08:44:28 +0700

    b12

---
+++ Python/Basic/b12.py
@@ -0,0 +1,5 @@
+# Nhập vào 3 số a, b, c. Tính và in ra d = (a + b)^c
+# Nếu d là số trong khoảng từ 100 - 200 thì in ra True, ngược lại in ra False
+a, b, c = list(map(float, input("Nhap a, b, c: ").split()))
+d = (a + b)**c
+print(100 <= d <= 200)
