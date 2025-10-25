597a01ce58cdd619d92346d569fa16f2a47deb9c
nvk205 <nvk.khanh205@gmail.com>
2025-10-25 09:32:50 +0700

    b22

--- Python/Basic/b22.py
+++ Python/Basic/b22.py
@@ -1,4 +1,4 @@
-# Nhập điểm toán, văn, anh.
+# Nhập điểm toán, văn, anh
 # Nếu điểm đúng quy tắc (trong khoảng từ 0 - 10), ta tính điểm trung bình rồi tiến hành xét:
 # Nếu điểm trung bình lớn hơn hoặc bằng 8, toán hoặc văn lớn hơn hoặc bằng 8 và không có điểm nào dưới 6.5 thì in ra “Học sinh giỏi”
 # Nếu không đủ điều kiện học sinh giỏi ta xét nếu điểm trung bình lớn hơn hoặc bằng 6.5, toán hoặc văn lớn hơn hoặc bằng 6.5 và không có điểm nào dưới 5 thì in ra “Học sinh khá”
